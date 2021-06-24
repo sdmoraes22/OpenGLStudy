ba01b27724bce9042961208ab9b0990a4f238113
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-06-23 23:00:43 -0300

    Vertex Array Buffer Abstraction

--- OpenGL/src/Main.cpp
+++ OpenGL/src/Main.cpp
@@ -8,6 +8,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 
 struct ShaderProgramSource 
@@ -137,15 +138,13 @@ int main(void)
             2, 3, 0
         };
 
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
-
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
-        GLCall(glEnableVertexAttribArray(0));
-        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
         IndexBuffer ib(indices, 6);
 
@@ -174,7 +173,7 @@ int main(void)
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.3f, 0.8, 1.0f));
 
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
