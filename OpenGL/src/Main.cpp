51585348ba54883e5d1b81b6b920c19f2631190b
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-07-06 20:22:44 -0300

    Rendering Multiple Objects in Open GL

--- OpenGL/src/Main.cpp
+++ OpenGL/src/Main.cpp
@@ -56,10 +56,10 @@ int main(void)
     {
 
         float positions[] = {
-            100.0f, 100.0f, 0.0f, 0.0f,
-             200.0f, 100.0f, 1.0f, 0.0f,
-             200.0f, 200.0f, 1.0f, 1.0f,
-             100.0f, 200.0f, 0.0f, 1.0f
+            -50.0f, -50.0f, 0.0f, 0.0f,
+             50.0f, -50.0f, 1.0f, 0.0f,
+             50.0f,  50.0f, 1.0f, 1.0f,
+            -50.0f,  50.0f, 0.0f, 1.0f
         };
 
         unsigned int indices[] = {
@@ -82,7 +82,7 @@ int main(void)
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho<float>(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         
 
@@ -107,7 +107,8 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translation(200, 200, 0);
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -119,17 +120,24 @@ int main(void)
 
             ImGui_ImplGlfwGL3_NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
+            
+            
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMath4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8, 1.0f);
-            shader.SetUniformMath4f("u_MVP", mvp);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMath4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            va.Bind();
-            ib.Bind();
-
-            renderer.Draw(va, ib, shader);
 
             if (r > 1.0f)
                 increment = -0.05f;
@@ -139,7 +147,8 @@ int main(void)
             r += increment;
 
             {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
