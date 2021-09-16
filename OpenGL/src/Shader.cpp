fa965524058714d148b11b1955b2f64ae5fb4fca
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-09-16 11:08:25 -0300

    Changes on the shader class

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -108,25 +108,29 @@ void Shader::Unbind() const
 
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
-    GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
+    int location = GetUniformLocation(name);
+    GLCall(glUniform4f(location, v0, v1, v2, v3));
 }
 
 void Shader::SetUniform1i(const std::string& name, int value)
 {
-    GLCall(glUniform1i(GetUniformLocation(name), value));
+    int location = GetUniformLocation(name);
+    GLCall(glUniform1i(location, value));
 }
 
 void Shader::SetUniform1f(const std::string& name, float value)
 {
-    GLCall(glUniform1f(GetUniformLocation(name), value));
+    int location = GetUniformLocation(name);
+    GLCall(glUniform1f(location, value));
 }
 
 void Shader::SetUniformMath4f(const std::string& name, const glm::mat4& matrix)
 {
-    GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
+    int location = GetUniformLocation(name);
+    GLCall(glUniformMatrix4fv(location, 1, GL_FALSE, &matrix[0][0]));
 }
 
-int Shader::GetUniformLocation(const std::string& name)
+int Shader::GetUniformLocation(const std::string& name) const
 {
     if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
         return m_UniformLocationCache[name];
